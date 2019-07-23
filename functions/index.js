0c38815f8199499ef10ba7f6a7442599591f6d22
James Aspinall <aspin@me.com>
2019-07-23 17:36:53 +1000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,7 @@ const firebase = require("firebase");
 const app = require("express")();
 admin.initializeApp();
 
-const config = {
-	apiKey: "AIzaSyBBWzSFeDALPFG7Doe1Y62z834kYoV2POU",
-	authDomain: "socialape-a8077.firebaseapp.com",
-	databaseURL: "https://socialape-a8077.firebaseio.com",
-	projectId: "socialape-a8077",
-	storageBucket: "socialape-a8077.appspot.com",
-	messagingSenderId: "453461361078",
-	appId: "1:453461361078:web:bb538747c1461267"
-};
+
 
 firebase.initializeApp(config);
 const db = admin.firestore();
